0e006b4d957c5b3cd01bc2942aee9136a87caaa6
BananaJoh <bananajoh@t-online.de>
2020-05-28 18:11:23 +0200

    App: Optimize imports

--- Android/app/src/main/java/de/bananajoh/sv650overlay/OverlayService.java
+++ Android/app/src/main/java/de/bananajoh/sv650overlay/OverlayService.java
@@ -8,7 +8,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
 import android.os.Binder;
